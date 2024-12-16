3ab847b7ab6e182a05b035f7b0100505c7349de3
Sharad Banga <sharadiit03@gmail.com>
2024-12-16 13:56:30 +0530

    updation file

---
+++ async100/two.js
@@ -0,0 +1,48 @@
+// const fs = require("fs");
+
+
+// const con1 = fs.readFileSync("./a.txt","utf-8");
+// console.log(con1);
+
+
+// const con2  = fs.readFileSync("./b.txt","utf-8");
+// console.log(con2);
+
+// //this is synchronous approach
+
+// // one by one
+
+// /**
+//  * 
+//  *
+//       task 1
+//         \/
+//       task 2
+//         \/
+//       task 3
+
+
+//  * 
+//  */
+
+
+
+function sum(a,b){
+    return a+b;
+}
+
+function multi(a,b){
+  return a*b;
+}
+
+
+function doOperations(a,b,fxn){
+  let ans = fxn(a,b);
+  return ans;
+}
+
+
+// console.log(sum(a,b));
+// console.log(multi(a,b));
+
+console.log(doOperations(2,3,multi));
