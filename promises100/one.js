6dfe13f06655c27b763c27c865350167a54f12ca
Sharad Banga <sharadiit03@gmail.com>
2025-06-28 11:46:02 +0530

    adding collections

--- promises100/one.js
+++ promises100/one.js
@@ -1,39 +1,24 @@
-//classes in JS
 class Rectangle{
 
-  //constructor defined in class
-  constructor(width, height, color){
+
+  constructor(width , height, color){
     this.width = width;
     this.height = height;
     this.color = color;
   }
 
-  //area function
   area(){
     return this.width*this.height;
   }
 
-  //paint function
-  paint(){
+  rang(){
     return this.color;
   }
 
 }
+
 const j = new Rectangle(2,3,"red");
 
 console.log(j.area());
-console.log(j.paint());
 
-
-//more classes 
-
-//date class
-const now = new Date();
-console.log(now.getMonth());
-
-//Map class
-
-const map = new Map();
-map.set('name','sharad');
-console.log(map.get('name'));
-
+console.log(j.rang());
--- promises100/three.js
+++ promises100/three.js
@@ -13,4 +13,3 @@ function call(content){
 }
 
 new Promise(readFile).then(call)
-
