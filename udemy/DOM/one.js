d955760a831325fc3edb54e6d52428794247b187
Sharad-Banga <sharadiit03@gmail.com>
2025-12-13 20:19:34 +0530

    learning DOM-basics-day-1

--- udemy/DOM/one.js
+++ udemy/DOM/one.js
@@ -86,13 +86,85 @@
 
 
 // let input = document.getElementById("feedbackInput");
-// let button = document.getElementById("b");
+// let form = document.getElementById("example-8");
 // let display = document.getElementById("feedbackDisplay");
-// button.addEventListener('click',(event)=>{
+// form.addEventListener('submit',(event)=>{
 //    event.preventDefault();  
-//     display.innerHTML = input.target.change.value;
+//     display.innerHTML = input.value;
     
 // })
 
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+//1
+// const p = document.getElementById('myParagraph');
+// const btn = document.getElementById('changeTextButton');
+
+// btn.addEventListener('click',()=>{
+//   p.innerText = "jindabaad";
+// })
+
+
+
+//2
+// const item = document.getElementById('citiesList').firstElementChild;
+// const btn = document.getElementById('HighlightFirstCity');
+
+// btn.addEventListener('click',()=>{
+//     if(item.classList.contains("high")){
+//       item.classList.remove('high');
+//     }
+//     else{
+//       item.classList.add('high');
+//     }
+// })
+
+
+//3
+// const itm = document.getElementById('coffeeType');
+// const btn = document.getElementById('changeOrder');
+
+// btn.addEventListener('click',()=>{
+//   itm.innerText = "Espresso"
+// })
+
+
+// //4
+// const list = document.getElementById("shoppingList");
+// const btn = document.getElementById('addNewItem');
+
+// btn.addEventListener('click',()=>{
+//   let newEle = document.createElement('li');
+//   newEle.innerText = "eggs";
+//   list.appendChild(newEle);
+// })
+
+//5
+// const list = document.getElementById('shoppingList-5');
+// const btn = document.getElementById('removeItem');
+
+// btn.addEventListener('click',()=>{
+//   list.lastChild.remove();
+// })
+
+//7
+
+// const list = document.getElementById('teaList');
+
+// list.addEventListener('click',(e)=>{
+//     alert(e.target.textContent)
+// })
+
+
+// const inp = document.getElementById('feedbackInput');
+// const btn = document.getElementById('b');
+// const dis = document.getElementById('feedbackDisplay');
+
+// btn.addEventListener('click',(e)=>{
+//   dis.innerText = inp.value;
+//   e.preventDefault();
+
+// })
