976cdf9863721a06826ae83c67f0ad02d4379941
Sharad-Banga <sharadiit03@gmail.com>
2026-01-08 15:31:13 +0530

    icon maker in DOM

--- 12days/day11/script11.js
+++ 12days/day11/script11.js
@@ -1,8 +1,27 @@
 let inptext = document.querySelector('.in');
 let distext = document.querySelector('.main-text');
 let rnge  = document.querySelector('.rnge');
+let mainbg = document.querySelector('.main-box');
 
 let fnt = document.querySelector('#font');
+
+let bg = document.querySelector('.bg-color');
+let fntclr = document.querySelector('.font-color');
+
+mainbg.style.backgroundColor = bg.value;
+distext.style.color = fntclr.value;
+
+fntclr.addEventListener('input',(e)=>{
+  distext.style.color = e.target.value;
+})
+
+
+bg.addEventListener('input',(e)=>{  
+  mainbg.style.backgroundColor = e.target.value;
+})
+
+
+
 
 distext.style.fontFamily = fnt.value;
 distext.textContent = inptext.value;
